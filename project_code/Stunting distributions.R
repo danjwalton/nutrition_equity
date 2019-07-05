345e82b9c1390e6c1efaee30ef4b2a518832d7cd
danwdevinit <dan.walton@devinit.org>
2019-07-05 16:42:56 +0100

    At Gini calc

--- project_code/Stunting distributions.R
+++ project_code/Stunting distributions.R
@@ -1,12 +1,12 @@
 required.packages <- c("reshape2","ggplot2","WDI","data.table")
 lapply(required.packages, require, character.only=T)
 
-wd <- "G:/My Drive/Work/GitHub/p20_indicator_time_trends"
+wd <- "G:/My Drive/Work/GitHub/nutrition_equity"
 wd2 <- "G:/My Drive/Work/GitHub/p20_indicator_time_trends/data/DHSauto"
 
 setwd(wd)
 
-dhsmeta<- fread("data/dhs_meta_data20190524.csv")
+dhsmeta<- fread("project_data/dhs_meta_data20190524.csv")
 dhsmeta<- subset(dhsmeta, Recode.Structure.!="DHS-I")
 
 dhsmeta$Country.[which(dhsmeta$Country.=="Cape Verde")]<-"Cabo Verde"
@@ -15,13 +15,23 @@ dhsmeta$Country.[which(dhsmeta$Country.=="Congo Democratic Republic")]<-"Congo, 
 dhsmeta$Country.[which(dhsmeta$Country.=="Egypt")]<-"Egypt, Arab Republic of"
 dhsmeta$Country.[which(dhsmeta$Country.=="Gambia")]<-"Gambia, The"
 dhsmeta$Country.[which(dhsmeta$Country.=="Yemen")]<-"Yemen, Republic of"
-#Afghanistan, Cambodia, Equatorial Guinea and Eritrea have had DHS surveys but don't have PovcalNet data
 names(dhsmeta)[which(names(dhsmeta)=="Country.")] <- "CountryName"
 
 dhsmeta$filename=paste0(dhsmeta$dhs_cc,"HR",dhsmeta$dhs_recode_code,"DT")
 dhsmeta=dhsmeta[which(!is.na(dhsmeta$dhs_cc)),]
 
 dhsmeta2 <- unique(dhsmeta[,c("CountryName","surveyyr","filename")])
+
+ginicalc <- function(P,L){
+  dP <- diff(P)
+  dL <- diff(L)
+  fL <- head(L,-1)
+  dPL <- dP*dL
+  dPfL <- dP*fL
+  area <- sum(dPL) + sum(dPfL)
+  gini <- 1-2*area
+  return(gini)
+}
 
 ####Run function####
 setwd(wd2)
@@ -41,6 +51,7 @@ missing.br = c(
   "rwbr7afl.RData",
   "slbr71fl.RData",
   "snbr50fl.RData",
+  "tgbr71fl.RData",
   "tzbr7ifl.RData", 
   "ugbr72fl.RData"
 )
@@ -50,7 +61,6 @@ rdatas = substr(rdatas,1,nchar(rdatas)-6)
 dataList <- list()
 dataIndex <- 1
 pb = txtProgressBar(max=nrow(dhsmeta),style=3)
-# Loop through every povcalcut
 for(i in 1:nrow(dhsmeta2)){
   dhsmetasubset <- dhsmeta2[i]
   country <- tolower(substr(dhsmetasubset$filename,1,2))
@@ -59,8 +69,7 @@ for(i in 1:nrow(dhsmeta2)){
   subphase <- substr(dhsmeta2$filename,5,5)
   rdata_name = paste0(country,recode,phase,"fl")
   if(!(rdata_name %in% rdatas)){ next; }
-  #message(paste(rdata_name,povcal_subset$RequestYear))
-  if(exists("pr")){rm(pr)}
+  if(exists("pr")){ rm(pr)}
   pr_patha <- paste0(country,"pr",phase)
   pr_path <- paste0(tolower(pr_patha),"fl.RData")
   load(pr_path)
@@ -78,7 +87,7 @@ for(i in 1:nrow(dhsmeta2)){
       br <- as.data.table(data)
       remove(data)
       dat <- as.data.table(br$hw5/100)
-    }
+    } else { next; }
   }
   dat <- dat[complete.cases(dat)]
   if(nrow(dat)==0){next;}
@@ -88,9 +97,12 @@ for(i in 1:nrow(dhsmeta2)){
   dat$z <- dat$V1 + abs(min(dat$V1))
   dat$L <- dat$z/sum(dat$z)
   dat$L <- cumsum(dat$L)
-  dataList[[i]] <- as.data.table(cbind(dhsmetasubset$CountryName, dhsmetasubset$surveyyr, dat$P, dat$L))
+  dataList[[i]] <- as.data.table(cbind(country=dhsmetasubset$CountryName, year=dhsmetasubset$surveyyr, file=dhsmetasubset$filename, P=dat$P, L=dat$L))
   setTxtProgressBar(pb,i)
 }
 
+alldist <- rbindlist(dataList)
+alldist.stats <- alldist[, .(Gini = ginicalc(as.numeric(P),as.numeric(L))), by=.(country,year,file)]
 
-
+setwd(wd)
+fwrite(alldist.stats, "project_data/Stunting gini stats.csv")
